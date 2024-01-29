8112607e90ac855861db3b87519755a40ad5c62b
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-01-29 14:05:09 +0300

    для проверки ошибки

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -3,7 +3,6 @@ import os
 
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
-
 import isodate
 
 
@@ -26,7 +25,7 @@ docs: https://developers.google.com/youtube/v3/docs/channels/list
 сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
 '''
 # channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # HighLoad Channel
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 printj(channel)
 
--- src/channel.py
+++ src/channel.py
@@ -8,15 +8,27 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
+        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.info_channel = json.dumps(self.channel, indent=2, ensure_ascii=False)
+        self.id = self.channel["items"][0]["id"]
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        #в json файле информации о канале нет поля url адреса канала, поэтому я составила структуру url сама
+        #Если я сделала это неправильно, то пожалуйста объясните по какому методу YOUTUBE API можно найти url адрес самого канала
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        info_channel = json.dumps(channel, indent=2, ensure_ascii=False)
-        print(info_channel)
 
+    @classmethod
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
+        return youtube
 
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
