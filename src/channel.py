8bf736b3b7112c9fb90f0fb432121d30157118e1
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-02-10 22:00:27 +0300

    Итоговый вариант домашнего задания №3

--- src/channel.py
+++ src/channel.py
@@ -15,12 +15,51 @@ class Channel:
         self.id = self.channel["items"][0]["id"]
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]["description"]
-        #в json файле информации о канале нет поля url адреса канала, поэтому я составила структуру url сама
-        #Если я сделала это неправильно, то пожалуйста объясните по какому методу YOUTUBE API можно найти url адрес самого канала
         self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
         self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+
+
+    def __str__(self) -> str:
+        return f'{self.title} (https://www.youtube.com/channel/{self.__channel_id})'
+
+
+    def __add__(self, other):
+        self.subscriber_count_2 = Channel('UCwHL6WHUarjGfUM_586me8w').subscriber_count
+        _add = int(self.subscriber_count) + int(self.subscriber_count_2)
+        return _add
+
+
+    def __sub__(self, other):
+         _sub_1 = int(self.subscriber_count) - int(self.subscriber_count_2)
+         return _sub_1
+
+    def __sub__(self, other):
+        _sub_2 = int(self.subscriber_count_2) - int(self.subscriber_count)
+        return _sub_2
+
+    def __gt__(self, other):
+        _gt = int(self.subscriber_count) > int(self.subscriber_count_2)
+        return _gt
+
+    def __ge__(self, other):
+        _ge = int(self.subscriber_count) >= int(self.subscriber_count_2)
+        return _ge
+
+    def __lt__(self, other):
+        _lt = int(self.subscriber_count) < int(self.subscriber_count_2)
+        return _lt
+
+    def __le__(self, other):
+        _le = int(self.subscriber_count) <= int(self.subscriber_count_2)
+        return _le
+
+    def __eq__(self, other):
+        _eq = int(self.subscriber_count) == int(self.subscriber_count_2)
+        return _eq
+
+
 
 
     @classmethod
@@ -33,6 +72,10 @@ class Channel:
         return self.__channel_id
 
     def to_json(self, file_name):
+        moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
         chanel_info = json.loads(self.info_channel)
-        with open(file_name, 'w') as file:
-            json.dump(chanel_info, file)
+        with open(file_name, 'w', encoding="utf-8") as file:
+            json.dump(f'{chanel_info}\n', file, ensure_ascii=False, indent=2)
+            json.dump(f'Название канала: {moscowpython.title}\n', file, ensure_ascii=False, indent=2)
+            json.dump(f'Количество просмотров: {moscowpython.video_count}\n', file, ensure_ascii=False)
+            json.dump(f'URL адрес канала: {moscowpython.url}\n', file, ensure_ascii=False)
