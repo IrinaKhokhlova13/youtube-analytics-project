2e71a03bc7e243c96c378c4bd0b8003e4d535e4c
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-02-20 18:38:05 +0300

    Bтоговый вариант домашнего задания #6

--- src/video.py
+++ src/video.py
@@ -9,13 +9,22 @@ class Video:
     def __init__(self, id_video: str) -> None:
         self.id_video = id_video
         self.youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=id_video).execute()
-        self.info_video = json.dumps(self.video_response, indent=2, ensure_ascii=False)
-        self._id = self.video_response['items'][0]['id']
-        self.title = self.video_response['items'][0]['snippet']['title']
-        self.url_video = f"https://www.youtube.com/watch/{self.id_video}"
-        self.view_Count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_Count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=id_video).execute()
+            self.info_video = json.dumps(self.video_response, indent=2, ensure_ascii=False)
+            self._id = self.video_response['items'][0]['id']
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url_video = f"https://www.youtube.com/watch/{self.id_video}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+        except:
+            self.id_video = id_video
+            self.title = None
+            self.url_video = None
+            self.view_count = None
+            self.like_count = None
+
 
     def __str__(self):
         return self.title
